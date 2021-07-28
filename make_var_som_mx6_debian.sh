4226de1ab5e49f47a901331d9d24b602d3a64721
Robert Wittek <robo-w@dragon.wien>
2021-07-28 22:16:54 +0200

    update script to debian buster

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -26,8 +26,8 @@ readonly SCRIPT_START_DATE=`date +%Y%m%d`
 readonly LOOP_MAJOR=7
 
 # default mirror
-readonly DEF_DEBIAN_MIRROR="http://httpredir.debian.org/debian"
-readonly DEB_RELEASE="stretch"
+readonly DEF_DEBIAN_MIRROR="http://ftp.tu-graz.ac.at/mirror/debian"
+readonly DEB_RELEASE="buster"
 readonly DEF_ROOTFS_TARBAR_NAME="rootfs.tar.gz"
 
 ## base paths
@@ -41,8 +41,8 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 ## LINUX kernel: git, config, paths and etc
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
-readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="ec7d7d8ccb4960ee382c97f3b08db16086382d4b"
+readonly G_LINUX_KERNEL_BRANCH="imx_5.4.70_2.3.0_var01"
+readonly G_LINUX_KERNEL_REV="2a54a41aaa9e614d45f307666235537229af3bf0"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx6dl-var-som-cap.dtb imx6dl-var-som-res.dtb imx6dl-var-som-solo-cap.dtb imx6dl-var-som-solo-res.dtb imx6dl-var-som-solo-vsc.dtb imx6dl-var-som-vsc.dtb imx6q-var-dart.dtb imx6q-var-som-cap.dtb imx6q-var-som-res.dtb imx6q-var-som-vsc.dtb imx6qp-var-som-cap.dtb imx6qp-var-som-res.dtb imx6qp-var-som-vsc.dtb'
 
